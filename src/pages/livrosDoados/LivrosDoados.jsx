da8d5299f32cd9453f61c1b06e44c944fdd8b462
Weynio-Santana <weyniovierasantana@gmail.com>
2025-04-03 14:49:23 -0300

    Adição post e get api

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,20 +1,48 @@
 import livroProtagonista from'../../assets/livroProtagonista.png'
 import s from'./livrosDoados.module.scss'
+import { useEffect,useState } from 'react'
+import axios from 'axios'
 
 export default function LivrosDoados (){
+
+    const [livros,setLivros] = useState([])
+
+    const getLivros = async() => {
+        const resposta = await axios.get("https://desafio2-api-livros.onrender.com/livros")
+        setLivros(resposta.data)
+    }
+    
+    useEffect(()=>{
+        getLivros()
+    },[])
+
 
     return(
             <section className={s.LivrosDoados}>
                     <h2>Livros Doados</h2>
                     <section className={s.CardsLivrosDoados}>
-                        <section>
+
+                        {
+                            livros.map((item) =>(
+                                <section> 
+                                    <img src = {item.image_url} alt={`Titulo do Livro ${item.titulo}`} />
+                                    <h3> {item.titulo}</h3>
+                                    <p>{item.autor}</p>
+                                    <p>{item.categoria}</p>
+                                </section>    
+
+
+                            ) )
+                        }
+
+                        {/* <section>
                             <img src={livroProtagonista} alt="Imagem do livro (O protagonista)" />
                             <div>
                             <h3>O protagonista</h3>
                             <p>Susanne Andrade</p>
                             <p>Ficção</p>
                             </div>
-                        </section>
+                        </section> */}
                     </section>
             </section> 
             
--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,22 +1,57 @@
 import iconelivro from'../../assets/iconelivro.png'
 import s from'./queroDoar.module.scss'
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function QueroDoar (){
 
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [image_url, setImage_Url] = useState("")
+
+    const capturaTitulo = (e) => {   
+        setTitulo(e.target.value)
+    }   
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) => {
+        setAutor(e.target.value)
+    }
+
+    const capturaImage = (e) => {
+        setImage_Url(e.target.value)
+    }
+
+    const enviarDados = async() => {
+
+        const dadosPEnviar ={
+            titulo,
+            categoria,
+            autor,
+            image_url
+        }
+
+        await axios.post("https://desafio2-api-livros.onrender.com/doar", dadosPEnviar)
+        
+
+    }  
     return(
         <section className={s.queroDoarSection}>
             <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
-            <form action="">
+            <form onSubmit={(e) => e.preventDefault()}>
                 <div>
                     <img src={iconelivro} alt="Icone com Livro aberto e borda azul" />
                     <h2>Informações do Livro</h2>
                 </div>
 
-                <input type="text" name="" id=""  placeholder='Título'/>
-                <input type="text" name="" id=""  placeholder='Categoria'/>
-                <input type="text" name="" id=""  placeholder='Autor'/>
-                <input type="text" name="" id=""  placeholder='Link da Imagem'/>
-                <input type="submit" value="Doar" className={s.buttonDoar}/>
+                <input type="text"  placeholder='Título' onChange={capturaTitulo} required/>
+                <input type="text"  placeholder='Categoria' onChange={capturaCategoria} required/>
+                <input type="text"  placeholder='Autor' onChange={capturaAutor} required/>
+                <input type="text"  placeholder='Link da Imagem' onChange={capturaImage} required  />
+                <input type="submit" value="Doar" className={s.buttonDoar} onClick={enviarDados} required/>
 
 
             </form>
